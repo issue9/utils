822e84cf780af9e900f007714d8119b5fe0b970f
caixw <xnotepad@gmail.com>
2020-03-21 17:47:26 +0800

    feat: 添加了一些用于获取运行时数据的函数
    CurrentDir 相当于 __DIR__
    CurrentLine 相当于 __LINE__
    CurrentFunction 相当于 __FUNCTION__
    CurrentFile 相当于 __LINE__

--- file.go
+++ file.go
@@ -6,6 +6,9 @@ import (
 	"go/format"
 	"io/ioutil"
 	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
 )
 
 // FileExists 判断文件或是文件夹是否存在
@@ -29,4 +32,51 @@ func DumpGoSource(path string, content []byte) error {
 	}
 
 	return ioutil.WriteFile(path, src, os.ModePerm)
+}
+
+// CurrentPath 获取`调用者`所在目录的路径
+//
+// 类似于部分语言的的 __DIR__ + "/" + path
+func CurrentPath(path string) string {
+	_, fi, _, _ := runtime.Caller(1)
+	return filepath.Join(filepath.Dir(fi), path)
+}
+
+// CurrentDir 获取`调用者`所在的目录
+//
+// 相当于部分语言的 __DIR__
+func CurrentDir() string {
+	_, fi, _, _ := runtime.Caller(1)
+	return filepath.Dir(fi)
+}
+
+// CurrentFile 获取`调用者`所在的文件
+//
+// 相当于部分语言的 __FILE__
+func CurrentFile() string {
+	_, fi, _, _ := runtime.Caller(1)
+	return fi
+}
+
+// CurrentLine 获取`调用者`所在的行
+//
+// 相当于部分语言的 __LINE__
+func CurrentLine() int {
+	_, _, line, _ := runtime.Caller(1)
+	return line
+}
+
+// CurrentFunction 获取`调用者`所在的函数名
+//
+// 相当于部分语言的 __FUNCTION__
+func CurrentFunction() string {
+	pc, _, _, _ := runtime.Caller(1)
+	name := runtime.FuncForPC(pc).Name()
+
+	index := strings.LastIndexByte(name, '.')
+	if index > 0 {
+		name = name[index+1:]
+	}
+
+	return name
 }
--- file_test.go
+++ file_test.go
@@ -4,6 +4,7 @@ package utils
 
 import (
 	"io/ioutil"
+	"path/filepath"
 	"testing"
 
 	"github.com/issue9/assert"
@@ -28,4 +29,46 @@ func TestDumpGoFile(t *testing.T) {
 	content, err := ioutil.ReadFile("./testdata/go.go")
 	a.NotError(err)
 	a.Equal(string(content), "var x = 1")
+}
+
+func TestCurrentPath(t *testing.T) {
+	a := assert.New(t)
+
+	dir, err := filepath.Abs("./file.go")
+	a.NotError(err).NotEmpty(dir)
+
+	d, err := filepath.Abs(CurrentPath("./file.go"))
+	a.NotError(err).NotEmpty(d)
+
+	a.Equal(d, dir)
+}
+
+func TestCurrentDir(t *testing.T) {
+	a := assert.New(t)
+
+	dir, err := filepath.Abs("./")
+	a.NotError(err).NotEmpty(dir)
+
+	a.Equal(CurrentDir(), dir)
+}
+
+func TestCurrentFile(t *testing.T) {
+	a := assert.New(t)
+
+	filename, err := filepath.Abs("./file_test.go")
+	a.NotError(err).NotEmpty(filename)
+
+	a.Equal(CurrentFile(), filename)
+}
+
+func TestCurrentFunction(t *testing.T) {
+	a := assert.New(t)
+
+	a.Equal(CurrentFunction(), "TestCurrentFunction")
+}
+
+func TestCurrentLine(t *testing.T) {
+	a := assert.New(t)
+
+	a.Equal(CurrentLine(), 73)
 }
